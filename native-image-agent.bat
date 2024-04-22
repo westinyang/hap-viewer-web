fe24ee90cd8c3c6e4a2628dbc6c1ebdd8b744af1
westinyang <517858177@qq.com>
2024-04-22 14:33:55 +0800

    fix: native-image-agent.bat
    Signed-off-by: westinyang <517858177@qq.com>

--- native-image-agent.bat
+++ native-image-agent.bat
@@ -1,1 +1,1 @@
-java -Dspring.aot.enabled=true -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -jar ./target/hap-viewer-web-1.0.jar
+java -Dspring.aot.enabled=true -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -jar ./target/hap-viewer-web-1.0.0.jar
